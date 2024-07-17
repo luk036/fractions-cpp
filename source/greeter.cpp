ab28ee90fcf6dc74498e123f4f23362e74a96878
Wai-Shing Luk <luk036@gmail.com>
2024-07-17 21:28:17 +0800

    remove greeter for termux

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -1,20 +1,20 @@
-#include <fmt/format.h>
+// #include <fmt/format.h>
 #include <greeter/greeter.h>
 
-using namespace fractions;
+// using namespace fractions;
 
-Fractions::Fractions(std::string _name) : name(std::move(_name)) {}
-
-std::string Fractions::greet(LanguageCode lang) const {
-  switch (lang) {
-    default:
-    case LanguageCode::EN:
-      return fmt::format("Hello, {}!", name);
-    case LanguageCode::DE:
-      return fmt::format("Hallo {}!", name);
-    case LanguageCode::ES:
-      return fmt::format("¡Hola {}!", name);
-    case LanguageCode::FR:
-      return fmt::format("Bonjour {}!", name);
-  }
-}
+// Fractions::Fractions(std::string _name) : name(std::move(_name)) {}
+//
+// std::string Fractions::greet(LanguageCode lang) const {
+//   switch (lang) {
+//     default:
+//     case LanguageCode::EN:
+//       return fmt::format("Hello, {}!", name);
+//     case LanguageCode::DE:
+//       return fmt::format("Hallo {}!", name);
+//     case LanguageCode::ES:
+//       return fmt::format("¡Hola {}!", name);
+//     case LanguageCode::FR:
+//       return fmt::format("Bonjour {}!", name);
+//   }
+// }
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,18 +1,18 @@
 #include <fractions/version.h>
-#include <greeter/greeter.h>
+// #include <greeter/greeter.h>
 
 #include <cxxopts.hpp>
 #include <iostream>
 #include <string>
-#include <unordered_map>
+// #include <unordered_map>
 
 auto main(int argc, char** argv) -> int {
-  const std::unordered_map<std::string, fractions::LanguageCode> languages{
-      {"en", fractions::LanguageCode::EN},
-      {"de", fractions::LanguageCode::DE},
-      {"es", fractions::LanguageCode::ES},
-      {"fr", fractions::LanguageCode::FR},
-  };
+  // const std::unordered_map<std::string, fractions::LanguageCode> languages{
+  //     {"en", fractions::LanguageCode::EN},
+  //     {"de", fractions::LanguageCode::DE},
+  //     {"es", fractions::LanguageCode::ES},
+  //     {"fr", fractions::LanguageCode::FR},
+  // };
 
   cxxopts::Options options(*argv, "A program to welcome the world!");
 
@@ -35,19 +35,19 @@ auto main(int argc, char** argv) -> int {
     return 0;
   }
 
-  if (result["version"].as<bool>()) {
-    std::cout << "Fractions, version " << FRACTIONS_VERSION << std::endl;
-    return 0;
-  }
+  // if (result["version"].as<bool>()) {
+  //   std::cout << "Fractions, version " << FRACTIONS_VERSION << std::endl;
+  //   return 0;
+  // }
 
-  auto langIt = languages.find(language);
-  if (langIt == languages.end()) {
-    std::cerr << "unknown language code: " << language << std::endl;
-    return 1;
-  }
+  // auto langIt = languages.find(language);
+  // if (langIt == languages.end()) {
+  //   std::cerr << "unknown language code: " << language << std::endl;
+  //   return 1;
+  // }
 
-  fractions::Fractions fractions(name);
-  std::cout << fractions.greet(langIt->second) << std::endl;
+  // fractions::Fractions fractions(name);
+  // std::cout << fractions.greet(langIt->second) << std::endl;
 
   return 0;
 }
