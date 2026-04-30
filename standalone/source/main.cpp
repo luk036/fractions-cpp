0e83b08e742e00ba88e099f003dae436a26182fb
Wai-Shing Luk <luk036@gmail.com>
2026-04-30 16:34:58 +0800

    replace push_back() and std::endl with emplace_back() and '\n'

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -31,23 +31,23 @@ auto main(int argc, char** argv) -> int {
     auto result = options.parse(argc, argv);
 
     if (result["help"].as<bool>()) {
-        std::cout << options.help() << std::endl;
+        std::cout << options.help() << '\n';
         return 0;
     }
 
     // if (result["version"].as<bool>()) {
-    //   std::cout << "Fractions, version " << FRACTIONS_VERSION << std::endl;
+    //   std::cout << "Fractions, version " << FRACTIONS_VERSION << '\n';
     //   return 0;
     // }
 
     // auto langIt = languages.find(language);
     // if (langIt == languages.end()) {
-    //   std::cerr << "unknown language code: " << language << std::endl;
+    //   std::cerr << "unknown language code: " << language << '\n';
     //   return 1;
     // }
 
     // fractions::Fractions fractions(name);
-    // std::cout << fractions.greet(langIt->second) << std::endl;
+    // std::cout << fractions.greet(langIt->second) << '\n';
 
     return 0;
 }
