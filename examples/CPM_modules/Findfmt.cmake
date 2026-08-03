43c7018575440718b10795caeb293a116658eeeb
Wai-Shing Luk <luk036@gmail.com>
2026-08-03 22:47:48 +0800

    chore: remove examples directory
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- examples/CPM_modules/Findfmt.cmake
+++
@@ -1,3 +0,0 @@
-include("/data/data/com.termux/files/home/github/cpp/fractions-cpp/examples/cmake/CPM_0.38.5.cmake")
-CPMAddPackage("NAME;fmt;GIT_TAG;12.1.0;GITHUB_REPOSITORY;fmtlib/fmt;OPTIONS;FMT_INSTALL YES")
-set(fmt_FOUND TRUE)
